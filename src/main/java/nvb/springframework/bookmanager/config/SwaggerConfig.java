2cdd49bc060541e7f619f36145352c96d84d3823
Erfan <erfannavab@gmail.com>
2022-10-22 13:10:20 +0330

    - fixed bugs

--- src/main/java/nvb/springframework/bookmanager/config/SwaggerConfig.java
+++ src/main/java/nvb/springframework/bookmanager/config/SwaggerConfig.java
@@ -2,7 +2,6 @@ package nvb.springframework.bookmanager.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.util.Predicates;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
